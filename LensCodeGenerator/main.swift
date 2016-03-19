efdf336754c414783d94546d5bfef5271b3575f5
Elviro Rocca <elviro.rocca@facile.it>
2016-03-19 10:30:41 +0100

    Initial Commit

---
+++ LensCodeGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LensCodeGenerator
+//
+//  Created by Elviro Rocca on 19/03/16.
+//  Copyright © 2016 exfalsoquodlibet.it. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
